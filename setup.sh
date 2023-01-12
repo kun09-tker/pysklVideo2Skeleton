b66264c3b90edd527e65738271fbac72deda6ba2
Lê Tấn Lộc <ltanloc97o@gmail.com>
2023-01-12 12:38:31 +0700

    start

---
+++ setup.sh
@@ -0,0 +1,29 @@
+echo "install dependencies: (use cu111 because colab has CUDA 11.1)"
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+
+echo "install mmcv-full thus we could use CUDA operators"
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
+
+echo "Download and install mmdetection"
+# Install mmdetection
+rm -rf mmdetection
+git clone https://github.com/open-mmlab/mmdetection.git
+cd mmdetection
+
+pip install -e .
+
+# Install some optional requirements
+pip install -r requirements/optional.txt
+
+echo "Download and install mmpose"
+cd ..
+rm -rf mmpose
+git clone https://github.com/open-mmlab/mmpose.git
+cd mmpose
+pip install -e .
+# Install some optional requirements
+pip install -r requirements/optional.txt
+cd ..
+
+echo "Update numpy"
+pip install --upgrade numpy
