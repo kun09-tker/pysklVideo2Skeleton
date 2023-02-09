b15559040bc8c81205df32c91067d4991a57b31a
Lê Tấn Lộc <ltanloc97o@gmail.com>
2023-02-09 13:00:46 +0700

    Update pose_extraction.py

--- pose_extraction.py
+++ pose_extraction.py
@@ -31,8 +31,8 @@ mmdet_root = 'mmdetection'
 mmpose_root = 'mmpose'
 
 args = abc.abstractproperty()
-args.det_config = f'{mmdet_root}/configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person.py'  # noqa: E501
-args.det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-person/faster_rcnn_r50_fpn_1x_coco-person_20201216_175929-d022e227.pth'  # noqa: E501
+args.det_config = f'{mmdet_root}/configs/swin/mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco.py'  # noqa: E501
+args.det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/swin/mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco/mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco_20210908_165006-90a4008c.pth'  # noqa: E501
 args.det_score_thr = 0.5
 args.pose_config = f'{mmpose_root}/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w32_coco_256x192.py'  # noqa: E501
 args.pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w32_coco_256x192-c78dce93_20200708.pth'  # noqa: E501
@@ -88,7 +88,8 @@ def pose_inference(args, frame_paths, det_results):
     prog_bar = mmcv.ProgressBar(len(frame_paths))
 
     num_frame = len(det_results)
-    num_person = round(np.mean([len(x) for x in det_results]))
+    num_person = max([len(x) for x in det_results])
+    print("\n\n max: ", num_person)
     kp = np.zeros((num_person, num_frame, 17, 3), dtype=np.float32)
 
     for i, (f, d) in enumerate(zip(frame_paths, det_results)):
