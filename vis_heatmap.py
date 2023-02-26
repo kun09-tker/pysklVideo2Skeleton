7385c973b5652317820784acdc3623a118520649
Lê Tấn Lộc <ltanloc97o@gmail.com>
2023-02-26 22:34:29 +0700

    Update vis_heatmap.py

--- vis_heatmap.py
+++ vis_heatmap.py
@@ -38,7 +38,9 @@ def vis_heatmaps(heatmaps, channel=-1, ratio=(8,8)):
     return heatmaps
 
 def to_pseudo_heatmap(anno, flag='keypoint'):
-    return np.array(get_pseudo_heatmap(anno, flag)).transpose(1, 0, 2, 3)
+    pseudo_heatmap = get_pseudo_heatmap(anno, flag)
+    pseudo_heatmap.transpose(1, 0, 2, 3)
+    return pseudo_heatmap
 #     limb_mapvis = vis_heatmaps(limb_heatmap, ratio=ratio)
 
 def to_heatmap(anno, flag='keypoint', ratio=(8,8)):
